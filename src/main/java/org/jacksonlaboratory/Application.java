4f9b08f423041736c1577dddfe7fced078a463b2
iimpulse <Michael.Gargano@jax.org>
2024-01-31 12:00:16 -0700

    reving to 0.5.8

--- src/main/java/org/jacksonlaboratory/Application.java
+++ src/main/java/org/jacksonlaboratory/Application.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.info.*;
     info = @Info(
             title = "ontology-service-${ontology}",
             description = "A restful service for the ${ontology} ontology.",
-            version = "0.5.6",
+            version = "0.5.8",
             contact = @Contact(name = "Michael Gargano", email = "Michael.Gargano@jax.org")
     )
 )
