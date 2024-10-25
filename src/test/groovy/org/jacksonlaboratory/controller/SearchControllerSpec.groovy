17e9727611ceb35cdc7a92aeb1b023aab379b9a4
iimpulse <Michael.Gargano@jax.org>
2024-10-25 10:55:37 -0600

    restricting search queries

--- src/test/groovy/org/jacksonlaboratory/controller/SearchControllerSpec.groovy
+++ src/test/groovy/org/jacksonlaboratory/controller/SearchControllerSpec.groovy
@@ -2,8 +2,10 @@ package org.jacksonlaboratory.controller
 
 import io.micronaut.context.annotation.Property
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
@@ -40,6 +42,19 @@ class SearchControllerSpec extends Specification {
         "arach" | [new OntologyTerm(TermId.of("HP:000003"), "fake name", "fake def", "comment", "", "", 0)] | 10
         "arach" | [new OntologyTerm(TermId.of("HP:000003"), "fake name", "fake def", "comment", "", "", 0)] | -1
     }
+
+    void "should 404 search #q and return nothing"() {
+        when:
+        def response = client.toBlocking().retrieve(HttpRequest.GET('/api/hp/search?q='+ q + "&limit=" + limit), Map.class)
+        then:
+        def e = thrown(HttpClientResponseException)
+        e.status.getCode() == 400
+        where:
+        q | res | limit
+        ")arach" | [] | 10
+        "arach%3B" | [] | -1
+    }
+
 
     @MockBean(TermService)
     TermService termService() {
